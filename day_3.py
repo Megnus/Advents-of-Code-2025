1c368982c1e29b990a730c7324d7c97f9dc94928
Megnus <megnus@hotmail.com>
2025-12-12 11:19:00 +0100

    Update day_3.py

--- day_3.py
+++ day_3.py
@@ -13,19 +13,20 @@ def part_1():
                 if tot > max_tot:
                     max_tot = tot
         sum_tot += max_tot
-
-
     return sum_tot
+
 
 def max_to_have_left(str_num, left):
     str_len = len(str_num) + 1
     s = str_num[:str_len - left]
     return s
 
+
 def the_largest_index(str_num, left):
     ar = max_to_have_left(str_num, left)
     i, m = max(enumerate(list(ar)), key=lambda x: x[1])
     return i, m, str_num[i + 1:]
+
 
 def recursive_num(str_num):
     max_num = ''
@@ -34,11 +35,13 @@ def recursive_num(str_num):
         max_num += str(m)
     return int(max_num)
 
+
 def part_2():
     sum_tot = 0
     for d in data:
         sum_tot += recursive_num(d)
     return sum_tot
+
 
 print('part 1:', part_1())
 print('part 2:', part_2())
