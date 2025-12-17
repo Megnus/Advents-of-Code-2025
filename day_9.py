50c8f87e1c7d8dc76593d0e56981217e51993c4d
megnus <megnus@hotmail.com>
2025-12-18 00:27:04 +0100

    Day 9 part 1 done.

--- day_9.py
+++ day_9.py
@@ -1,10 +1,22 @@
 import aoc
+import itertools
 
 data = aoc.get_data(True)
-print(data)
+data = [tuple(int(i) for i in d.split(',')) for d in data]
+
 
 def part_1():
-    return None
+    comb = list(itertools.combinations(data, 2))
+    #print(comb)
+    comb_dist = [(A, B, (abs(A[0] - B[0]) + 1) * (abs(A[1] - B[1]) + 1)) for A, B in comb]
+    """
+    print(comb_dist)
+    for cd in comb_dist:
+        print(cd)
+    """
+    max_coordinate = max(comb_dist, key=lambda c: c[2])
+    A, B, area = max_coordinate
+    return area
 
 
 def part_2():
