c70fe5ec7e2c9dba7f979a6d290a44c695fdbd8f
Megnus <megnus@hotmail.com>
2026-01-28 13:52:39 +0100

    Finished day 11 part 1.

--- day_11.py
+++ day_11.py
@@ -1,15 +1,42 @@
 import aoc
+from collections import deque
 
 data = aoc.get_data(True)
-print(data)
+data_dict = dict()
+
+
+def rearrange_data():
+	for d in data:
+		d = d.replace(':', '')
+		d = d.split(' ')
+		data_dict[d[0]] = d[1:]
+
+
+def bfs():
+	count = 0
+	dq = deque(['you'])
+	visited = set()
+	while dq:
+		next_step = dq.popleft()
+		visited.add(next_step)
+		new_paths = data_dict[next_step]
+		for np in new_paths:
+			if np == 'out':
+				count += 1
+			else:
+				dq.append(np)
+	return count
+
 
 def part_1():
-    return None
+    return bfs()
 
 
 def part_2():
     return None
 
+
+rearrange_data()
 
 print('part 1:', part_1())
 print('part 2:', part_2())
